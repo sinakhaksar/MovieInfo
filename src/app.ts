6bcaa57c37758fe01d019670058ef3645825e6ee
sina khaksar <sinakhaksar3@gmail.com>
2025-10-20 10:13:57 +0330

    changed app to connect to db

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
 import express from "express";
 import "dotenv/config";
 
+import sequelize from "./database.js";
+
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-	console.log(`server is running on localhost:${PORT}`);
-});
+const startServer = async () => {
+	try {
+		await sequelize.authenticate();
+		console.log("DB connected: ✅");
+		app.listen(PORT, () => {
+			console.log(`server connected: ✅`);
+		});
+	} catch (err) {
+		console.log("ERROR 🟥: ", err);
+	}
+};
+
+startServer();
