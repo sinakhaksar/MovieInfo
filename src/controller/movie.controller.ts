6a502701c1109f2ad3bd335688a65448d399741a
sina khaksar <sinakhaksar3@gmail.com>
2025-10-20 12:36:42 +0330

    calling service from controller

--- src/controller/movie.controller.ts
+++ src/controller/movie.controller.ts
@@ -1,12 +1,17 @@
 import type { NextFunction, Request, Response } from "express";
+import { movieService } from "../services/movie.service.js";
 
-const movieFinder = (req: Request, res: Response, next: NextFunction) => {
+const movieFinder = async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		const { title } = req.params;
 
+		if (!title) return; // to avoid typeError in MovieFinder
+		// calling service to do business logic
+		const titleFromService = await movieService.MovieFinder(title);
 		return res.status(200).json({
 			msg: "ok",
 			title,
+			titleFromService, // this is for test and will be removed later
 		});
 	} catch (err) {
 		next(err);
