47c0c4b50d43f83405d971d8e5bb1c679b3b9bbb
sina khaksar <sinakhaksar3@gmail.com>
2025-10-20 14:50:00 +0330

    Fixed service.

--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -1,7 +1,45 @@
+import { notFoundError } from "../error/httpError.js";
+import { MovieRepository } from "../repositories/movie.repositories.js";
+
 export const movieService = {
 	async MovieFinder(title: string) {
-		// call the repo heare
+		const movie = await MovieRepository.findMovieByTitle(title);
+		if (!movie) {
+			// call the omdbapi
+			const apiMovie = await omdbapi(title);
+			if (!apiMovie) throw new notFoundError("movie not Found");
 
-		return title.toLocaleUpperCase();
+			// it makes it simpler for repository
+			const data: any = {
+				title: apiMovie.Title,
+				year: Number(apiMovie.Year),
+				director: apiMovie.Director,
+				plot: apiMovie.Plot,
+				imdbRating: Number(apiMovie.imdbRating),
+			};
+			await MovieRepository.createMovie(data);
+			data.cached = false;
+
+			return data;
+		}
+		movie.dataValues.cached = true;
+		delete movie.dataValues.id;
+		delete movie.dataValues.createdAt;
+		delete movie.dataValues.updatedAt;
+		return movie.dataValues;
 	},
+};
+
+const omdbapi = async (title: string) => {
+	const data = await fetch(
+		`http://www.omdbapi.com/?apikey=${process.env.apikey}&t=${title}`,
+	);
+
+	// Parse the body stream into JSON
+	const movie = await data.json();
+
+	if (movie.Response === "False") return null;
+	else {
+		return movie;
+	}
 };
