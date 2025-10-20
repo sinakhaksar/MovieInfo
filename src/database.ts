26d511496769a3b72e8255b6cd1ff04e40f28e21
sina khaksar <sinakhaksar3@gmail.com>
2025-10-20 10:12:47 +0330

    connected to db

---
+++ src/database.ts
@@ -0,0 +1,10 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+	process.env.DBNAME as string,
+	process.env.DBUSERNAME as string,
+	process.env.DBPASSWORD as string,
+	{ dialect: "mysql" },
+);
+
+export default sequelize;
